e51a964f0a0adb76ca9baa88f30cf342c2f6aea5
TheApo98 <73966912+TheApo98@users.noreply.github.com>
2021-05-15 12:41:40 +0300

    Added a new script for easier testing

---
+++ run.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+# *** Be careful , error handling is NOT implemented ***
+
+# Assign new human-readable variables for the arguments  
+example=$1
+# echo $lexfile
+# echo $example
+
+# Compile the .y file
+bison -d -v -r all myanalyzer.y
+
+# Compile the .l file
+flex mylexer.l
+
+# Compile the .c file
+gcc -o mycompiler myanalyzer.tab.c lex.yy.c cgen.c -lfl
+
+# Run the executable with the .pi file
+./mycompiler < examples/"$example" > "$example".c

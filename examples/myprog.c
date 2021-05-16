5896d704b28c23f1c84ffd2c9fbf9c28d724747c
TheApo98 <73966912+TheApo98@users.noreply.github.com>
2021-05-16 22:30:37 +0300

    Added more examples

---
+++ examples/myprog.c
@@ -0,0 +1,8 @@
+#include "pilib.h"
+
+const char *message = "Hello world!\n";
+
+int main() {
+  writeString(message);
+  return 0;
+}
---
+++ examples/useless.c
@@ -0,0 +1,25 @@
+#include "pilib.h"
+
+const int N = -100;
+
+int a, b; 
+
+int cube(int i) { 
+   return i*i*i; 
+} 
+
+int add(int n, int k) { 
+   int j;         
+        
+   j = (N-n) + cube(k); 
+   writeInt(j); 
+   return j;
+} 
+ 
+int main() { 
+  
+   a = readInt(); 
+   b = readInt(); 
+   
+   add(a, b);
+} 
